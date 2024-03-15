b7ae898a514b85a0dbf55bec4ea53a7992375631
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2024-03-15 17:40:39 +0530

    Create FIRE SENSOR.ino

---
+++ lab1/b. FIRE SENSOR./FIRE SENSOR.ino
@@ -0,0 +1,24 @@
+
+const int buzzerPin = 9;
+const int fireSensorPin = 8;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(fireSensorPin, INPUT);
+}
+void loop()
+{
+  int fireValue = digitalRead(fireSensorPin);
+  Serial.println(fireValue);
+  if(fireValue == 0)
+  {
+    digitalWrite(buzzerPin, HIGH);
+    delay(5000);
+  }
+  else
+  {
+    digitalWrite(buzzerPin, LOW);
+  }
+  delay(500);
+}
