765bd213cd2e68a0e19f96184f3235b2e7c37d6b
Pratik Rajput <110278577+prateekrjt14@users.noreply.github.com>
2024-04-08 16:47:06 +0530

    Create ballSwitch.ino

---
+++ lab4/c. Ball switch/ballSwitch.ino
@@ -0,0 +1,17 @@
+const int ballSwitchPin = 2; // Digital pin connected to ball switch
+const int ledPin = 13;       // Digital pin connected to LED
+
+void setup() {
+  pinMode(ballSwitchPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  int ballState = digitalRead(ballSwitchPin);
+  
+  if (ballState == HIGH) {
+    digitalWrite(ledPin, HIGH); // Turn on LED
+  } else {
+    digitalWrite(ledPin, LOW);  // Turn off LED
+  }
+}
