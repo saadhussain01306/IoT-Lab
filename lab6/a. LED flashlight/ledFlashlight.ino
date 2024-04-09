7a409bab8e9d94df5e6ac61871b6b733dfc1bc42
Pratik Rajput <110278577+prateekrjt14@users.noreply.github.com>
2024-04-09 21:18:49 +0530

    Update ledFlashlight.ino

--- lab6/a. LED flashlight/ledFlashlight.ino
+++ lab6/a. LED flashlight/ledFlashlight.ino
@@ -1,12 +1,24 @@
-const int ledPin = 13; // Digital pin connected to LED
+int ledPin = 13;
+int buttonPin = 7;
+int buttonState = 0;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);      // sets the digital pin 13 as output
+  pinMode(buttonPin, INPUT);      // sets the digital pin 7 as input
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH); // Turn on LED
-  delay(500); // Adjust the delay for desired flashing frequency
-  digitalWrite(ledPin, LOW); // Turn off LED
-  delay(500); // Adjust the delay for desired flashing frequency
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
 }
