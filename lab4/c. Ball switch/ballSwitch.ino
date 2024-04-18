eb5d7120d8ed73e66eb1683e753e20db18186883
Pratik Rajput <110278577+prateekrjt14@users.noreply.github.com>
2024-04-18 17:16:21 +0530

    Update ballSwitch.ino

--- lab4/c. Ball switch/ballSwitch.ino
+++ lab4/c. Ball switch/ballSwitch.ino
@@ -1,5 +1,5 @@
 const int ballSwitchPin = 2; // Digital pin connected to ball switch
-const int ledPin = 13;       // Digital pin connected to LED
+const int ledPin = 13;       // Digital pin connected to LED (it is an in-built LED in UNO)
 
 void setup() {
   pinMode(ballSwitchPin, INPUT);
