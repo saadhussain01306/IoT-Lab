81fbf42c3e158bb94ae6526d94a546597244bf28
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2024-04-20 08:03:55 +0530

    Update mercuryTiltSwitch.ino

--- lab4/a. Mercury tilt switch/mercuryTiltSwitch.ino
+++ lab4/a. Mercury tilt switch/mercuryTiltSwitch.ino
@@ -2,6 +2,7 @@ const int tiltSwitchPin = 2; // Digital pin connected to tilt switch
 const int ledPin = 13;       // Digital pin connected to LED (it is an in-built LED in UNO)
 
 void setup() {
+  Serial.begin(9600);
   pinMode(tiltSwitchPin, INPUT);
   pinMode(ledPin, OUTPUT);
 }
@@ -10,8 +11,11 @@ void loop() {
   int tiltState = digitalRead(tiltSwitchPin);
   
   if (tiltState == HIGH) {
+    Serial.println("HIGH");
     digitalWrite(ledPin, HIGH); // Turn on LED
   } else {
     digitalWrite(ledPin, LOW);  // Turn off LED
+    Serial.println("LOW");
   }
+  delay(1000);
 }
