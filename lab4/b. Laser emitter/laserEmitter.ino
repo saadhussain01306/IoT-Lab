3d7f25f0deb6766313533961f83057ba81ffe2b8
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2024-04-20 08:20:02 +0530

    Update laserEmitter.ino

--- lab4/b. Laser emitter/laserEmitter.ino
+++ lab4/b. Laser emitter/laserEmitter.ino
@@ -2,14 +2,18 @@ const int laserPin = 8; // Digital pin connected to laser module
 
 void setup() {
   pinMode(laserPin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // Turn on the laser for 1 second
   digitalWrite(laserPin, HIGH);
+  Serial.println("HIGH");
   delay(1000);
+  
   
   // Turn off the laser for 1 second
   digitalWrite(laserPin, LOW);
+  Serial.println("low");
   delay(1000);
 }
