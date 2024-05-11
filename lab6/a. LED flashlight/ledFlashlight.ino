0b0fddbf5a637b9d72b078322a98e53ef149c2f8
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2024-05-11 07:57:00 +0530

    Update ledFlashlight.ino

--- lab6/a. LED flashlight/ledFlashlight.ino
+++ lab6/a. LED flashlight/ledFlashlight.ino
@@ -2,14 +2,18 @@ const int flashLightPin = 8; // Digital pin connected to flashLight module
 
 void setup() {
   pinMode(flashLightPin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // Turn on the laser for 1 second
   digitalWrite(flashLightPin, HIGH);
+  Serial.println("High");
   delay(1000);
+  
   
   // Turn off the laser for 1 second
   digitalWrite(flashLightPin, LOW);
+  Serial.println("Low");
   delay(1000);
 }
